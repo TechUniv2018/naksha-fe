7b32c726b93dff48347e07997d560c1dc6dc85c7
anand <ananddeb23@gmail.com>
2018-03-16 18:05:49 +0530

    New design

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: "airbnb",
-  env: {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "jsx-a11y/anchor-is-valid": ["error", {
+      "components": ["Link"],
+      "specialLink": ["to"]
+    }]
+  },
+  "env": {
     "browser": true
   }
 };
